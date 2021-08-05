37c7682a6ca66a5d752aed494c25bc6b481068ce
eshdata <84449335+eshdata@users.noreply.github.com>
2021-08-05 09:55:31 +0530

    Update Section7.js

--- src/Components/Home/Section7/Section7.js
+++ src/Components/Home/Section7/Section7.js
@@ -10,7 +10,7 @@ export default class Section7 extends Component {
     return (
       <div className="section7-container">
         <div className="section7-left">
-          <h1>With Pangaia <br></br>Pangaia x AIRINK</h1>
+          <h1>With Pangaia <br></br>Pangaia x AIR-INK®</h1>
           <p>
             We partnered with Pangaia to create water-based black ink from air pollution.
           </p>
